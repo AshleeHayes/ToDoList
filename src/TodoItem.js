c9fd4779d7b5989da514c12367d3736cfd4b848c
Ashlee Hayes <hayesashlee88@gmail.com>
2019-10-21 12:14:45 -0400

    finished todo list

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,24 @@
 import React from "react"
-import todosData from "./todosData";
 
 function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed} />
-            <p>{props.item.text}</p>
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
         </div>
     )
 }
+
+// when events fire they receive an event property- so can't use just props.handlechange it has to be (event) => props.handlechange
 
 export default TodoItem
